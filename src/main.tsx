eab423137f3e4645668ba94cc6629e03507e2457
Hamidreza maharlu <maharlu.hamidreza@gmail.com>
2023-04-12 15:05:44 +0200

    add/chakra Ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import DataContextProvider from "./state/data-context";
+import {ChakraProvider} from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <DataContextProvider>
+            <ChakraProvider>
             <App/>
+            </ChakraProvider>
         </DataContextProvider>
     </React.StrictMode>,
 )

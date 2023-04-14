7350ed27c8777aaf8b5668d028ec2fe3ebc0c2d6
Hamidreza maharlu <maharlu.hamidreza@gmail.com>
2023-04-14 12:52:32 +0200

    fix/genre selected and sorted

--- src/components/Genres/Genres.tsx
+++ src/components/Genres/Genres.tsx
@@ -7,7 +7,7 @@ function GenresAside() {
 
     return (
         <>
-            {genres.map(item=><HStack key={item.id} onClick={()=>setQueryParams({genres:item.id})} marginY={5}><Img width={5} height={5}
+            {genres.map(item=><HStack key={item.id} onClick={()=>setQueryParams({...QueryParams,genres:item.id})} marginY={5}><Img width={5} height={5}
                                                       src={item.image_background}/><Link fontWeight={item.id === QueryParams?.genres ? "bold" : "normal"} textDecoration={item.id === QueryParams?.genres ? "underline" : "unset"}>{item.name}</Link></HStack>)}
         </>
     );
